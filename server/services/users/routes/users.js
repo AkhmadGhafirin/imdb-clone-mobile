70b1d15b27718db6321aeac6360f60edc53726a7
AkhmadGhafirin <ahmadghafirin@gmail.com>
2023-06-22 12:20:49 +0700

    add findAll and findOne for users

--- server/services/users/routes/users.js
+++ server/services/users/routes/users.js
@@ -6,5 +6,7 @@ const router = express.Router()
 
 router.post('/login', UserController.login)
 router.post('/register', UserController.register)
+router.get('/', UserController.findAll)
+router.get('/:_id', UserController.findById)
 
 module.exports = router

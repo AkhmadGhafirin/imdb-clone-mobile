f966576c09945fc0e671e5550112c03530d6267e
AkhmadGhafirin <ahmadghafirin@gmail.com>
2023-07-09 13:06:05 +0700

    cretae .env for orchestrator

--- server/orchestrator-express/config/redis.js
+++ server/orchestrator-express/config/redis.js
@@ -1,6 +1,6 @@
 const Redis = require("ioredis");
 module.exports = new Redis({
   port: 12623,
-  host: "redis-12623.c292.ap-southeast-1-1.ec2.cloud.redislabs.com",
-  password: "OxEqKoarETQC41qIenLPC8GLRW3NzE7g"
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASSWORD
 })
